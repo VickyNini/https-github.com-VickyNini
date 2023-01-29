b12d72759ffe6aa1d492d4f280deadf75621fab4
Victoria Meriño <mariavictoriamerinomacwilliam@gmail.com>
2023-01-29 00:29:50 -0300

    Argentina Programa 4.0, Proyecto Final

---
+++ js/boton.js
@@ -0,0 +1,19 @@
+document.getElementById('botonInstagram').onclick = function () {
+    console.log("Mostrar usuario ig");
+    document.getElementById("botonInstagram").innerHTML = "@Jenn.Nicols";
+}
+
+document.getElementById('botonTwitter').onclick = function () {
+    console.log("Mostrar usuario tw");
+    document.getElementById("botonTwitter").innerHTML = "@N.Jenn";
+}
+
+document.getElementById('botonFacebook').onclick = function () {
+    console.log("Mostrar usuario fb");
+    document.getElementById("botonFacebook").innerHTML = "Jany Nichols";
+}
+
+document.getElementById('botonLinkedin').onclick = function () {
+    console.log("Mostrar usuario li");
+    document.getElementById("botonLinkedin").innerHTML = "Nichols, Jannie";
+}
